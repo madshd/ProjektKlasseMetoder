f7396baeff4e8776c835dfe1a7d45359a120de46
Mads D <42411664+madshd@users.noreply.github.com>
2024-03-13 13:03:40 +0100

    Moved to a test

--- src/TestMads/TeamApp.java
+++ src/TestMads/TeamApp.java
@@ -2,7 +2,7 @@ package TestMads;
 
 import java.util.Scanner;
 
-static Scanner input = new Scanner(System.in);
+
 public class TeamApp {
     public static void main(String[] args) {
         for (int i = 0; i < 1;) {
@@ -10,19 +10,19 @@ public class TeamApp {
             System.out.println("2 - Withdraw: ");
             System.out.println("3 - Deposit: ");
             System.out.println("4 - Exit menu: ");
-            int menuChoice = input.nextInt();
-            if (menuChoice == 1) {
-
-            }
-            if (menuChoice == 2) {
-
-            }
-            if (menuChoice == 3) {
-
-            }
-            if (menuChoice == 4) {
-
-            }
+////            int menuChoice = input.nextInt();
+//            if (menuChoice == 1) {
+//
+//            }
+//            if (menuChoice == 2) {
+//
+//            }
+//            if (menuChoice == 3) {
+//
+//            }
+//            if (menuChoice == 4) {
+//
+//            }
         }
     }
 }
